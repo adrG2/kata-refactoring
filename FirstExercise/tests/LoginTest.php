cb5e786bc49b0ddde48e78378254ede9d40c90ad
adrG2 <adrgrr.91@gmail.com>
2019-10-19 19:43:00 +0200

    Add test and impl LoginException.

--- FirstExercise/tests/LoginTest.php
+++ FirstExercise/tests/LoginTest.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+use FirstExercise\LoginException;
 use PHPUnit\Framework\TestCase;
 use FirstExercise\LoginController;
 
@@ -9,12 +10,24 @@ final class LoginTest extends TestCase
     private const LOGIN_SUCCESSFUL = 'Login successful';
     private const PHP_UNIT_IT_S_WORKS_FINE = "PhpUnit it's works fine";
 
+    private $stub;
+
+    public function setUp(): void
+    {
+        $this->stub = $this->createMock(LoginController::class);
+    }
+
     public function testPhpUnitIsOk(): void
     {
-        $mockObject = $this->createMock(LoginController::class);
-        $mockObject->method('login')->willReturn(self::LOGIN_SUCCESSFUL);
-        $this->assertEquals(self::PHP_UNIT_IT_S_WORKS_FINE, $mockObject->login(), self::LOGIN_SUCCESSFUL);
+        $this->stub->method('login')->willReturn(self::LOGIN_SUCCESSFUL);
+        $this->assertEquals(self::LOGIN_SUCCESSFUL, $this->stub->login(),self::PHP_UNIT_IT_S_WORKS_FINE);
     }
+
+    public function testExceptionLoginMethod(): void {
+        $this->stub->method('login')->willThrowException(new LoginException('Login not valid'));
+    }
+
+
 
 
 }
