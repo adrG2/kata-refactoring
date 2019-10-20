cd2c24d0e0ae106aaed1d541f920afa85f3ee2f8
adrG2 <adrgrr.91@gmail.com>
2019-10-20 14:00:21 +0200

    Add tests adn modify composer.json

---
+++ SecondExercise/tests/DrawTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+use FirstExercise\LoginException;
+use PHPUnit\Framework\TestCase;
+use FirstExercise\LoginController;
+
+final class DrawTest extends TestCase
+{
+
+    private $stub;
+
+    public function setUp(): void
+    {
+        //$this->stub = $this->createMock();
+    }
+
+
+}
---
+++ ThirdExercise/tests/EncodeTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+use FirstExercise\LoginException;
+use PHPUnit\Framework\TestCase;
+use FirstExercise\LoginController;
+
+final class EncodeTest extends TestCase
+{
+
+    private $stub;
+
+    public function setUp(): void
+    {
+        //$this->stub = $this->createMock(LoginController::class);
+    }
+
+}
