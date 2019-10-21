f6729fe85881b223f0494eca92e5edf79533ddf8
adrG2 <adrgrr.91@gmail.com>
2019-10-21 18:46:08 +0200

    Add domain user

---
+++ FirstExercise/src/refactored/domain/User.php
@@ -0,0 +1,50 @@
+<?php
+
+
+class User
+{
+    private $email;
+    private $password;
+
+
+    public function __construct($email, $password)
+    {
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password): void
+    {
+        $this->password = $password;
+    }
+
+
+
+}
